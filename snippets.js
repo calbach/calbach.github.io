aaff569b98332578b5f0b20eac5f02ece4a91399
CH Albach <calbach@users.noreply.github.com>
2018-05-15 23:18:38 -0700

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,3 +1,5 @@
+const octokit = Octokit();
+
 const until = new Date();
 until.setDate(until.getDate() - ((until.getDay() - 1) % 7));
 until.setHours(0);
@@ -7,6 +9,9 @@ until.setMilliseconds(0);
 
 const since = new Date(until - 7 * 24 * 60 * 60 * 1000);
 
+const contentsEl = document.getElementById("contents");
+
+// TODO: Grab from all recent repos.
 octokit.repos.getCommits({
   owner: "all-of-us",
   repo: "workbench",
@@ -15,5 +20,17 @@ octokit.repos.getCommits({
   since: since,
   until: until
 }).then(result => {
-  console.log(JSON.stringify(result["data"].map(r => r.commit.message.split('\n')[0])));
+  const prRe = /^(.*) \(#(\d+)\)$/;
+  result.data
+    .map(r => {
+      const msg = r.commit.message.split('\n')[0];
+      const match = msg.match(prRe);
+      if (!match) {
+        return msg;
+      }
+      return `[#${match[2]}](https://github.com/all-of-us/workbench/pull/${match[2]}) ${match[1]}`;
+    })
+    .forEach(line => {
+      contentsEl.textContent += `- ${line}\n`;
+    });
 })
